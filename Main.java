ba06dbedce982c1cb330f9673810ea0227a561db
rqpedro <queirooz@hotmail.com>
2026-02-22 15:25:40 -0300

    Contagem até 10

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Estudando Github");
+
+        for (int i = 1; i <= 10 ; i ++) {
+            System.out.println(i);
+        }
     }
 }
